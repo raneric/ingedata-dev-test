df314c44013c951e2de6cc2e137dda9dd4433829
Eric RANDRIANARISOA <raneric@gmail.com>
2025-07-16 21:19:58 +0300

    adding github action

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,8 @@ import app from './app.js';
 import dotenv from 'dotenv';
 
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'localhost';
 
-app.listen(PORT, () => {
-  console.log("listening on http://localhost:3000");
+app.listen(PORT, HOST, () => {
+  console.log(`listening on ${HOST}:${PORT}`);
 })

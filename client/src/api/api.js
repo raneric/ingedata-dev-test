bab8a469bf2e47a24e04c1509152efc9bd5d6fc5
raneric <raneric@gmail.com>
2026-03-31 19:03:19 +0300

    fix vite env

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { AppPath } from '../utils/appConstant';
 import { getAccessToken, setAccessToken } from '../services/authServices';
 
-const BASE_URL = process.end.API_URL;
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
